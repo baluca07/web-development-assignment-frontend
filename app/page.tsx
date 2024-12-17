667b5909f7783cdd30eab52aca7619fc4dadf6bb
Bagoly Luca Enikő <bagolyluca03@gmail.com>
2024-12-17 23:01:45 +0100

    Modified HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,23 @@
 "use client";
 
+
 export default function Home() {
     return (
         <div>
             <h1>Welcome in the frontend app!</h1>
             <p className={`text-center`}>Get data from http://localhost:8080/api</p>
-            <p className={`font-bold italic`}>See data:</p>
-            <ul>
-                <li><button onClick={()=> window.location.href = "../departments"}>Departments</button></li>
-                <li><button onClick={()=> window.location.href = "../employees"}>Employees</button></li>
+            <p className={`text-center`}><b className={`text-red-600`}>IMPORTANT:</b> The app should be launched
+                at <a
+                    href="http://localhost:3000">http://localhost:3000</a> , not from an IP address (e.g.:
+                http://192.168.0.6:3000)!</p>
+            <p className={`font-bold italic text-center my-3`}>See data:</p>
+            <ul className={`text-center`}>
+                <li>
+                    <button onClick={() => window.location.href = "../departments"}>Departments</button>
+                </li>
+                <li>
+                    <button onClick={() => window.location.href = "../employees"}>Employees</button>
+                </li>
             </ul>
         </div>
     );
