1ab0fc1225fe4f282baa96dd8a4b598a93f86792
Bagoly Luca Enikő <bagolyluca03@gmail.com>
2024-12-17 22:49:46 +0100

    Fixed token issues

--- hooks/useToken.ts
+++ hooks/useToken.ts
@@ -1,13 +1,19 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const TOKEN_KEY = 'jwt-token';
 
 export const useToken = () => {
-    const [token, setTokenState] = useState<string | null>(
-        localStorage.getItem(TOKEN_KEY)
-    );
+    const [token, setTokenState] = useState<string | null>(null);
+
+    useEffect(() => {
+        // Ellenőrizd, hogy a böngészői környezetben fut-e a kód
+        if (typeof window !== 'undefined') {
+            const storedToken = localStorage.getItem(TOKEN_KEY);
+            setTokenState(storedToken);
+        }
+    }, []);
 
     const setToken = (response: Response) => {
         response.text().then((tokenText) => {
@@ -19,12 +25,14 @@ export const useToken = () => {
     };
 
     const getToken = (): string | null => {
-        return localStorage.getItem(TOKEN_KEY);
+        return typeof window !== 'undefined' ? localStorage.getItem(TOKEN_KEY) : null;
     };
 
     const removeToken = () => {
-        localStorage.removeItem(TOKEN_KEY);
-        setTokenState(null);
+        if (typeof window !== 'undefined') {
+            localStorage.removeItem(TOKEN_KEY);
+            setTokenState(null);
+        }
     };
 
     return { token, setToken, getToken, removeToken };
